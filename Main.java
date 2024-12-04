03bc0a60ec3390e5deeae4b8ae9591812c3284c5
vladkim91 <vkcoding21@gmail.com>
2024-12-04 11:44:52 -0800

    recursive method completed

--- Main.java
+++ Main.java
@@ -28,12 +28,12 @@ class Main
 
     //Uncomment this section to see your recursive method in action!
     
-    // System.out.println("\n\nRecursive Output");
-    // System.out.println("-----------------");
-    // System.out.printf("Your investment will be $%.2f after %d year(s) at a rate of %.1f%%", recursiveInterestCalc(amount, rate, years), years, rate*100);
+     System.out.println("\n\nRecursive Output");
+     System.out.println("-----------------");
+     System.out.printf("Your investment will be $%.2f after %d year(s) at a rate of %.1f%%", recursiveInterestCalc(amount, rate, years), years, rate*100);
   }
 
-  /*iterativeInterestCalc method uses a for loop to calulate the new balance in your investment account.*/
+  /*iterativeInterestCalc method uses a for loop to calculate the new balance in your investment account.*/
   public static double iterativeInterestCalc(double amount, double rate, int years)
   { 
     /*Take a percentage of the amount to get the compound interest by multiplying the amount by the rate. Then add that compound interest to the amount to get the updated amount. Do this calculation for each year the investment stays in the account. For an investment in the account for 3 years, you would perform this calulation 3 times.*/
@@ -47,7 +47,13 @@ class Main
 
 
   //Start coding here!!
-  /*recursiveInterestCalc method uses recursion to calulate the new balance in your investment account*/
-  
+  /*recursiveInterestCalc method uses recursion to calculate the new balance in your investment account*/
+  public static double recursiveInterestCalc(double amount, double rate, int years) {
+    if (years == 0) {
+      return amount;
+    }
+    double newBalance = rate * amount + amount;
+    return recursiveInterestCalc(newBalance, rate, years - 1);
+  }
 
 }
